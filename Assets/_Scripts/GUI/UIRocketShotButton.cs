ae22eb140438c8ed43e22ea6585b8998cb0b3426
Alexander Evstratov <alexdrugev@me.com>
2023-04-04 18:15:14 +0300

    Add object pool for rockets.

--- Assets/_Scripts/GUI/UIRocketShotButton.cs
+++ Assets/_Scripts/GUI/UIRocketShotButton.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+using UnityEngine.Pool;
 
 namespace Evstr.GUI
 {
     public class UIRocketShotButton : UIBaseButton
     {
-        [SerializeField] private GameObject _rocketPrefab;
         [SerializeField] private GameObject _player;
 
         public override void OnClick()
         {
-            var rocket = Instantiate(_rocketPrefab);
-            rocket.transform.position = new Vector2(_player.transform.position.x, _player.transform.position.y - 0.24f);
+            GameObject rocket = ObjectPoolRocket.SharedInstance.GetPooledObject();
+            if (rocket != null)
+            {
+                rocket.transform.position = new Vector2(_player.transform.position.x, _player.transform.position.y - 0.24f);
+                rocket.SetActive(true);
+            }
         }
     }
 }
--- Assets/_Scripts/Rocket/RockerMovement.cs
+++ Assets/_Scripts/Rocket/RockerMovement.cs
@@ -12,6 +12,10 @@ namespace Evstr
         private void Update()
         {
             transform.Translate(Vector2.right * _speed * Time.deltaTime);
+            if (transform.position.x > 13.0f)
+            {
+                gameObject.SetActive(false);
+            }
         }
     }
 }
